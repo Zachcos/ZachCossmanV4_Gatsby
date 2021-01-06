5cc55790beb9528d5e0705d6fd20b87c542049da
Zach Cossman <Zachcos@gmail.com>
2021-01-06 02:55:49 -0500

    Add Helmet and GlobalStyle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import GlobalStyle from '../imports/globalStyle';
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <h1>This is our layout component</h1>
-      {children}
-    </div>
+    <>
+      <Helmet>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <title>Zach Cossman</title>
+        <script
+          src="https://kit.fontawesome.com/c3abb1e5c6.js"
+          crossOrigin="anonymous"
+        />
+      </Helmet>
+      <GlobalStyle />
+      <div>
+        <h1>This is our layout component</h1>
+        {children}
+      </div>
+    </>
   );
 }
