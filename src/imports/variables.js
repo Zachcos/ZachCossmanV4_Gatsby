6f63cf152966d8d4214d74b117bc4b4327bd286b
Zach Cossman <Zachcos@gmail.com>
2021-01-06 03:04:24 -0500

    Create variable file with color and font objects

---
+++ src/imports/variables.js
@@ -0,0 +1,14 @@
+export const color = {
+  dark: '#222834',
+  mid: '#38596b',
+  light: '#9eb0bb',
+  accent: '#52514e',
+};
+
+export const font = {
+  display: '5.61rem',
+  heading: '2.369rem',
+  subheading: '1.333rem',
+  subsubheading: '0.75rem',
+  body: '1rem',
+};
