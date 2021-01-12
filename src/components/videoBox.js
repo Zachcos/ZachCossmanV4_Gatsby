6b334d67d7f7def0c92740c5c536cfbea7d6a46f
Zach Cossman <Zachcos@gmail.com>
2021-01-12 16:55:06 -0500

    Update VideoBox component with lightGallery functionality and link to NCL footage

--- src/components/videoBox.js
+++ src/components/videoBox.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import $ from 'jquery';
 import { color } from '../imports/variables';
+import 'lightgallery';
+import 'lg-video';
+import '../css/lightgallery.css';
 
 const Wrapper = styled.div`
   align-items: center;
   display: flex;
   justify-content: center;
+  overflow: hidden;
   position: relative;
   &:hover {
     i {
       opacity: 1;
     }
   }
-  i {
-    color: ${color.light};
-    cursor: pointer;
-    opacity: 0.3;
-    position: absolute;
-    transition: opacity 0.2s ease-out;
+  a {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    i {
+      color: ${color.light};
+      cursor: pointer;
+      opacity: 0.3;
+      position: absolute;
+      transition: opacity 0.2s ease-out;
+    }
   }
   video {
     height: 100%;
@@ -27,11 +39,16 @@ const Wrapper = styled.div`
   }
 `;
 
-export default function VideoBox({ video, area }) {
+export default function VideoBox({ video, area, ref }) {
+  function onLightGallery(node) {
+    $(node).lightGallery();
+  }
   return (
-    <Wrapper style={{ gridArea: area }}>
-      <i className="far fa-6x fa-play-circle" />
-      <video src={video} autoPlay loop muted playsInline />
+    <Wrapper style={{ gridArea: area }} ref={onLightGallery}>
+      <a href="https://youtu.be/M7OKn9vBj9M">
+        <i className="far fa-6x fa-play-circle" />
+        <video src={video} autoPlay loop muted playsInline />
+      </a>
     </Wrapper>
   );
 }
