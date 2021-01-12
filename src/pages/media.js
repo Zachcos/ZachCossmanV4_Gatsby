75fd07155bc77e0ef20aa4cf2a93d249364c1c6b
Zach Cossman <Zachcos@gmail.com>
2021-01-12 13:04:00 -0500

    Add styling for initial media page layout

--- src/pages/media.js
+++ src/pages/media.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { color, device } from '../imports/variables';
+import DownloadBox from '../components/downloadBox';
+import TextBox from '../components/textBox';
+
+const Wrapper = styled.div`
+  align-items: center;
+  background: ${color.light};
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  grid-area: 1 / 1 / 3 / 3;
+  justify-content: center;
+  padding: 25px 50px;
+  position: relative;
+  z-index: 15;
+  @media ${device.phone} {
+    grid-area: 1 / 1 / 4 / 3;
+  }
+`;
 
 export default function Media() {
+  const details = {
+    heading: 'Placeholder',
+    subheading: 'something goes here',
+    subnote: '',
+  };
+
   return (
-    <div>
-      <h1>This will be the media page</h1>
-    </div>
+    <>
+      <Wrapper>
+        <h1>This will be the media page</h1>
+      </Wrapper>
+      <TextBox area="four" theme="medium" content={details} />
+      <DownloadBox area="five" theme="dark" />
+    </>
   );
 }
