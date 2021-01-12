261cf0784d02ae982761c32ef6146d7f8b220a93
Zach Cossman <Zachcos@gmail.com>
2021-01-12 13:53:47 -0500

    Add graphql query to pull images and add initial styling for galleries

--- src/pages/media.js
+++ src/pages/media.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { color, device } from '../imports/variables';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import $ from 'jquery';
+import { color, device, font } from '../imports/variables';
+import 'lightgallery';
+import 'lg-video';
+import '../css/lightgallery.css';
 import DownloadBox from '../components/downloadBox';
 import TextBox from '../components/textBox';
 
@@ -18,9 +24,34 @@ const Wrapper = styled.div`
   @media ${device.phone} {
     grid-area: 1 / 1 / 4 / 3;
   }
+  h2.gallery-header {
+    font-size: ${font.heading};
+    font-weight: ${font.bold};
+    width: 100%;
+  }
+  .photo-gallery {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    width: 100%;
+    a.thumb {
+      margin: 25px 10px;
+      max-height: 150px;
+      max-width: 150px;
+      width: 100%;
+      @media ${device.phone} {
+        max-height: 100px;
+        max-width: 100px;
+      }
+    }
+  }
 `;
 
-export default function Media() {
+export default function Media({ data }) {
+  function onLightGallery(node) {
+    $(node).lightGallery();
+  }
+
   const details = {
     heading: 'Placeholder',
     subheading: 'something goes here',
@@ -30,10 +61,35 @@ export default function Media() {
   return (
     <>
       <Wrapper>
-        <h1>This will be the media page</h1>
+        <h2 className="gallery-header">Headshots</h2>
+        <div className="photo-gallery" ref={onLightGallery}>
+          {data.images.nodes
+            .filter((item) => item.original.src.includes('headshot'))
+            .map((img) => (
+              <a href={img.original.src} key={img.id} className="thumb">
+                <Img fluid={img.fluid} alt="" />
+              </a>
+            ))}
+        </div>
       </Wrapper>
       <TextBox area="four" theme="medium" content={details} />
       <DownloadBox area="five" theme="dark" />
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    images: allImageSharp(sort: { fields: fixed___originalName, order: ASC }) {
+      nodes {
+        id
+        fluid(maxWidth: 250, maxHeight: 250, quality: 70) {
+          ...GatsbyImageSharpFluid
+        }
+        original {
+          src
+        }
+      }
+    }
+  }
+`;
