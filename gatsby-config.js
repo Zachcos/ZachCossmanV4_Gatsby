d266a3910185092ff28116139867d5eac17dfed0
Zach Cossman <Zachcos@gmail.com>
2021-01-12 13:16:13 -0500

    Add source-filesystem and plugin-sharp to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,19 @@ module.exports = {
     'babel-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-plugin-layout',
       options: {
         component: require.resolve('./src/components/layout.js'),
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
       },
     },
   ],
