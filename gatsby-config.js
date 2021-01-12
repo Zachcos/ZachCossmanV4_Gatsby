788438d6ef0ad6f202f2e5842b7bdd5424cdf084
Zach Cossman <Zachcos@gmail.com>
2021-01-12 14:11:11 -0500

    Install gatsby-transformer-json dependencies and add to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-transformer-json',
     {
       resolve: 'gatsby-plugin-layout',
       options: {
