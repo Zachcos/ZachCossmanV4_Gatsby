4a5b43f77776f7101e2b70fb84a308f1908f19ae
Zach Cossman <Zachcos@gmail.com>
2022-07-07 16:30:03 -0400

    Comment-out everything in index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,38 +1,41 @@
 import React from 'react';
 
 // Components
-import HeroText from '../components/heroText';
-import PhotoBox from '../components/photoBox';
-import TextBox from '../components/textBox';
-import VideoBox from '../components/videoBox';
-import DownloadBox from '../components/downloadBox';
+// import HeroText from '../components/heroText';
+// import PhotoBox from '../components/photoBox';
+// import TextBox from '../components/textBox';
+// import VideoBox from '../components/videoBox';
+// import DownloadBox from '../components/downloadBox';
 
 // Assets
-import headshot from '../../static/assets/headshot.jpg';
-import video from '../../static/assets/ncl_thumbnail.mp4';
+// import headshot from '../../static/assets/headshot.jpg';
+// import video from '../../static/assets/ncl_thumbnail.mp4';
 
-export default function Home() {
-  const gig = {
-    heading: 'Footloose',
-    subheading: 'Norwegian Cruise Lines',
-    subnote: 'Mon, May 23th',
-    info:
-      'Zach is currently playing Ren McCormack in Footloose on the Norwegian Joy.',
-  };
+const Home: React.FC = () => {
+  // const gig = {
+  //   heading: 'Footloose',
+  //   subheading: 'Norwegian Cruise Lines',
+  //   subnote: 'Mon, May 23th',
+  //   info: 'Zach is currently playing Ren McCormack in Footloose on the Norwegian Joy.',
+  // };
 
   return (
-    <>
-      <HeroText text="Zach Cossman" area="one" />
-      <PhotoBox image={headshot} area="two" />
-      <TextBox area="three" content={gig}>
-        <p className="body">
-          <br />
-          {gig.info}
-          <br />
-        </p>
-      </TextBox>
-      <VideoBox video={video} area="four" />
-      <DownloadBox area="five" theme="dark" />
-    </>
+    // <>
+    //   <HeroText text='Zach Cossman' area='one' />
+    //   <PhotoBox image={headshot} area='two' />
+    //   <TextBox area='three' content={gig}>
+    //     <p className='body'>
+    //       <br />
+    //       {gig.info}
+    //       <br />
+    //     </p>
+    //   </TextBox>
+    //   <VideoBox video={video} area='four' />
+    //   <DownloadBox area='five' theme='dark' />
+    // </>
   );
-}
+} ;
+
+export default Home;
+
+
