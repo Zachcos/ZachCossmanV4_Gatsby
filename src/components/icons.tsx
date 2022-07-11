6a0487cbc85985d9ec50fba2a78d29aa438e3324
Zach Cossman <Zachcos@gmail.com>
2022-07-11 12:57:57 -0400

    Create iconWrapper styled component

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,2 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { color } from '../imports/variables';
+
+const IconWrapper = styled.svg`
+  height: 50px;
+  width: 50px;
+`;
