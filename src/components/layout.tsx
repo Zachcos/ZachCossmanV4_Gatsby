d595e3e1af8baf4a5af78fee06fd6ae30c6e69e3
Zach Cossman <Zachcos@gmail.com>
2024-02-20 15:58:24 -0800

    Comment-out all code related to <IntroOverlay />

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,54 +1,56 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+// import { useEffect } from 'react';}
 import { Helmet } from 'react-helmet';
-import gsap from 'gsap';
+// import gsap from 'gsap';
 import GlobalStyle from '../imports/globalStyle';
 import GridWrapper from './gridWrapper';
-import IntroOverlay from './introOverlay';
+// import IntroOverlay from './introOverlay';
 import Nav from './nav';
+import IntroOverlay from './introOverlay';
 
 const Layout = ({ children }: ChildElementProp) => {
-  useEffect(() => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // useEffect(() => {
+  //   const vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-    const tl = gsap.timeline();
+  //   const tl = gsap.timeline();
 
-    // prevent flashing
-    gsap.to('body', { duration: 0, css: { visibility: 'visible' } });
+  //   // prevent flashing
+  //   gsap.to('body', { duration: 0, css: { visibility: 'visible' } });
 
-    tl.from('.container h1', {
-      duration: 1.8,
-      delay: 1,
-      ease: 'power4.out',
-      y: 120,
-      skewY: 7,
-    })
-      .to('.overlay-top', {
-        duration: 1.6,
-        stagger: 0.4,
-        ease: 'expo.inOut',
-        height: 0,
-      })
-      .to('.overlay-bottom', {
-        width: 0,
-        duration: 1.6,
-        delay: -0.8,
-        stagger: 0.4,
-        ease: 'expo.inOut',
-      })
-      .to('#intro-overlay', {
-        duration: 0,
-        css: { display: 'none' },
-      })
-      .to('#nav-wrap', {
-        duration: 0,
-        css: { zIndex: 100 },
-      })
-      .to('#nav-btn', {
-        duration: 0,
-        css: { zIndex: 200 },
-      });
-  }, []);
+  //   tl.from('.container h1', {
+  //     duration: 1.8,
+  //     delay: 1,
+  //     ease: 'power4.out',
+  //     y: 120,
+  //     skewY: 7,
+  //   })
+  //     .to('.overlay-top', {
+  //       duration: 1.6,
+  //       stagger: 0.4,
+  //       ease: 'expo.inOut',
+  //       height: 0,
+  //     })
+  //     .to('.overlay-bottom', {
+  //       width: 0,
+  //       duration: 1.6,
+  //       delay: -0.8,
+  //       stagger: 0.4,
+  //       ease: 'expo.inOut',
+  //     })
+  //     .to('#intro-overlay', {
+  //       duration: 0,
+  //       css: { display: 'none' },
+  //     })
+  //     .to('#nav-wrap', {
+  //       duration: 0,
+  //       css: { zIndex: 100 },
+  //     })
+  //     .to('#nav-btn', {
+  //       duration: 0,
+  //       css: { zIndex: 200 },
+  //     });
+  // }, []);
 
   return (
     <>
@@ -62,7 +64,7 @@ const Layout = ({ children }: ChildElementProp) => {
       </Helmet>
       <GlobalStyle />
       <GridWrapper>
-        <IntroOverlay />
+        {/* <IntroOverlay /> */}
         <Nav />
         {children}
       </GridWrapper>
