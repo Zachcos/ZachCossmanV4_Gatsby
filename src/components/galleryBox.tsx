438d24cd52cded26070299bbcba7eca62a0c6b01
Zach Cossman <zach@Zachs-MacBook-Pro.local>
2024-08-19 16:55:08 -0700

    Create galleryBox component

---
+++ src/components/galleryBox.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import LightGallery from 'lightgallery/react';
+import 'lightgallery/css/lightgallery.css';
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  position: relative;
+  .lg-react-element {
+    width: 100%;
+    height: 100%;
+  }
+  &:hover {
+    svg {
+      opacity: 1;
+    }
+  }
+  a {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    svg {
+      cursor: pointer;
+      opacity: 0.3;
+      position: absolute;
+      transition: opacity 0.15s ease-out;
+    }
+  }
+  video {
+    height: 100%;
+    object-fit: cover;
+    object-position: right;
+    width: 100%;
+  }
+`;
+
+interface Props {
+  video: string;
+  area: string;
+}
+
+const GalleryBox = ({ video, area }: Props) => {
+  return (
+    <Wrapper style={{ gridArea: area }}>
+      <LightGallery>
+        <a
+          href='https://youtu.be/M7OKn9vBj9M'
+          aria-label='video clip'
+          data-src='https://youtu.be/M7OKn9vBj9M'
+        >
+          <video src={video} autoPlay loop muted playsInline />
+        </a>
+      </LightGallery>
+    </Wrapper>
+  );
+};
+
+export default GalleryBox;
